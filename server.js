72a7632cb9c3b4471e1a0054773e7cc1a7730105
Brian rimi <107041935+remydev47@users.noreply.github.com>
2024-01-17 08:34:13 +0300

    refactors

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL)
+.then(() => console.log("Foodly Connected to Database"))
+.catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'))
-app.listen(port, () => console.log(`Foodly Backend is Running on Port ${port}`))
+app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is Running on Port ${process.env.PORT}!`))
