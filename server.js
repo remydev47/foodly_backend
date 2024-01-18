eda236c94ee26f291feb66b313f3096e6a66a753
Brian rimi <107041935+remydev47@users.noreply.github.com>
2024-01-18 09:00:28 +0300

    routecreateCategoryControl

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const CategoryRoute = require("./routes/Category");
 
 
 dotenv.config();
@@ -9,6 +10,11 @@ dotenv.config();
 mongoose.connect(process.env.MONGOURL)
 .then(() => console.log("Foodly Connected to Database"))
 .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("categories", CategoryRoute);
+
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(process.env.PORT || 6013, () => console.log(`Foodly Backend is Running on Port ${process.env.PORT}!`))
